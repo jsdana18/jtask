f1aa8668efd8a5f7e0edba38183bb3200fbcd1c0
mothergoose <mothergoosevery@gmail.com>
2021-07-31 13:44:15 +0800

    STEP 8: test again

--- spec/features/task_spec.rb
+++ spec/features/task_spec.rb
@@ -7,12 +7,12 @@ RSpec.feature 'test', type: :feature, driver: :chrome, js: true, slow: true do
     end
 
     scenario 'Tasks CRUD' do
-    #   expect(Task.count).to eq(1)
+      expect(Task.count).to eq(0)
     #   click_on('新增工作')
       fill_in('task[name]', with: '吃東西')
       fill_in('task[content]', with: '吃餅乾')
       click_button('新增工作')
-    #   expect(Task.count).to eq(2)
+      expect(Task.count).to eq(1)
     end
   end
 end
