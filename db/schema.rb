241faba4d685088cff24cabbb632bb71d9398a9d
mothergoose <mothergoosevery@gmail.com>
2021-08-08 16:38:23 +0800

    STEP 15: rspec features not ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_29_092858) do
+ActiveRecord::Schema.define(version: 2021_08_08_053037) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name", null: false
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 2021_07_29_092858) do
     t.datetime "end_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_tasks_on_name"
+    t.index ["priority"], name: "index_tasks_on_priority"
+    t.index ["state"], name: "index_tasks_on_state"
   end
 
   create_table "users", force: :cascade do |t|
--- spec/features/task_spec.rb
+++ spec/features/task_spec.rb
@@ -2,98 +2,86 @@ require 'rails_helper'
 
 RSpec.feature 'test', type: :feature, driver: :chrome, js: true, slow: true do
   describe 'Feature spec/工作測試，執行測試用指令：$ rspect spec/features' do
-    # before(:each) do
-    #   visit new_task_path
-    # end
-
-    scenario 'Tasks 新增工作並刪除' do
+    before(:each) do
       visit new_task_path
       expect(Task.count).to eq(0)
-    #   click_on('新增工作')
-      fill_in('task[name]', with: '吃東西')
-      fill_in('task[content]', with: '吃餅乾')
-      fill_in('task[end_date]', with: (Time.zone.now+3.days).to_date)
+      fill_in('task[name]', with: '追日劇')
+      fill_in('task[content]', with: '第 22 集')
+      fill_in('task[end_date]', with: (Time.zone.now+5.days).to_date)
       select("進行中", from: 'task[state]')
+      select("低", from: 'task[priority]')
       click_button('新增')
       expect(page).to have_content '工作新增成功。'
       expect(Task.count).to eq(1)
+
+      visit new_task_path
+      fill_in('task[name]', with: '追韓劇')
+      fill_in('task[content]', with: '神話第 25 集')
+      fill_in('task[end_date]', with: (Time.zone.now+2.days).to_date)
+      select("待處理", from: 'task[state]')
+      select("中", from: 'task[priority]')
+      click_button('新增')
+      expect(page).to have_content '工作新增成功。'
+      expect(Task.count).to eq(2)
+
+      visit new_task_path
+      fill_in('task[name]', with: '吃午餐')
+      fill_in('task[content]', with: '深夜食堂')
+      fill_in('task[end_date]', with: (Time.zone.now+6.days).to_date)
+      select("進行中", from: 'task[state]')
+      select("高", from: 'task[priority]')
+      click_button('新增')
+      expect(page).to have_content '工作新增成功。'
+      expect(Task.count).to eq(3)
+
+      visit new_task_path
+      fill_in('task[name]', with: '追美劇')
+      fill_in('task[content]', with: '紙牌第 7 集')
+      fill_in('task[end_date]', with: (Time.zone.now+1.days).to_date)
+      select("待處理", from: 'task[state]')
+      select("中", from: 'task[priority]')
+      click_button('新增')
+      expect(page).to have_content '工作新增成功。'
+      expect(Task.count).to eq(4)
+    end
+
+    scenario 'Tasks 新增工作並刪除' do
       visit task_index_path
-      click_on('刪除')
-      expect(Task.count).to eq(0)
+      click_on('刪除', match: :first)
+      expect(Task.count).to eq(3)
     end
 
     scenario 'Tasks 新增工作並編輯' do
-      visit new_task_path
-      expect(Task.count).to eq(0)
-      fill_in('task[name]', with: '追劇')
-      fill_in('task[content]', with: '第 69 集')
-      fill_in('task[end_date]', with: (Time.zone.now+3.days).to_date)
-      select("進行中", from: 'task[state]')
-      click_button('新增')
-      expect(page).to have_content '工作新增成功。'
-      expect(Task.count).to eq(1)
-
       visit task_index_path
-      click_on('編輯')
-      fill_in('task[name]', with: '追韓劇')
+      click_on('編輯', match: :first)
+      fill_in('task[name]', with: '追美劇')
       fill_in('task[content]', with: '第 70 集')
       fill_in('task[end_date]', with: (Time.zone.now+4.days).to_date)
       select("結束", from: 'task[state]')
       click_button('編輯')
       expect(page).to have_content '工作更新成功。'
-      expect(Task.first.name).to eq("追韓劇")
+      expect(Task.first.name).to eq("追美劇")
+      expect(Task.first.state).to eq("done")
     end
 
     scenario 'Tasks 新增工作並排序' do
-      visit new_task_path
-      expect(Task.count).to eq(0)
-      fill_in('task[name]', with: '追劇')
-      fill_in('task[content]', with: '第 22 集')
-      fill_in('task[end_date]', with: (Time.zone.now+2.days).to_date)
-      select("進行中", from: 'task[state]')
-      click_button('新增')
-      expect(page).to have_content '工作新增成功。'
-
-      visit new_task_path
-      fill_in('task[name]', with: '吃午餐')
-      fill_in('task[content]', with: '便當')
-      fill_in('task[end_date]', with: (Time.zone.now+1.days).to_date)
-      select("結束", from: 'task[state]')
-      click_button('新增')
-      expect(page).to have_content '工作新增成功。'
-      expect(Task.count).to eq(2)
-
       click_on('最新日期排前')
-      expect(page).to have_content("/吃午餐.*追劇/")
-      click_on('用ID排前')
-      expect(page).to have_content("/追劇.*吃午餐/")
+      expect(page).to match /吃午餐.+追日劇.+追韓劇/
+      click_on('依 ID 排前')
+      expect(page).to match /追日劇.*追韓劇.*吃午餐/m
+      click_on('依優先權高排前')
+      expect(page).to match /吃午餐.*追韓劇.*追美劇/m
       # expect(page).to have_content 'Success'
     end
 
-    scenario 'Tasks 新增工作並排序' do
-      visit new_task_path
-      expect(Task.count).to eq(0)
-      fill_in('task[name]', with: '追劇')
-      fill_in('task[content]', with: '第 22 集')
-      fill_in('task[end_date]', with: (Time.zone.now+3.days).to_date)
-      select("進行中", from: 'task[state]')
-      click_button('新增')
-      expect(page).to have_content '工作新增成功。'
 
-      visit new_task_path
-      fill_in('task[name]', with: '吃午餐')
-      fill_in('task[content]', with: '便當')
-      fill_in('task[end_date]', with: (Time.zone.now+3.days).to_date)
-      select("待處理", from: 'task[state]')
-      click_button('新增')
-      expect(page).to have_content '工作新增成功。'
-      expect(Task.count).to eq(2)
-
-      click_on('最新日期排前')
-      expect(page).to have_content("/吃午餐.*追劇/")
-      click_on('用ID排前')
-      expect(page).to have_content("/追劇.*吃午餐/")
-      # expect(page).to have_content 'Success'
+    scenario 'Tasks 新增工作並搜尋' do
+      visit q_task_index_path
+      fill_in('q', with: '劇')
+      select("進行中", from: 'state')
+      click_button('搜尋工作')
+      expect(page).to have_content "追日劇"
+      # expect(page).to have_content "/追日劇.*進行中.*吃午餐.*進行中/"
     end
     
 
